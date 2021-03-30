df10ba4aab5cbb99cc59fbc9cfe02d8daf8e0621
Akshai Addaguduru <addagudurua1@udayton.edu>
2021-03-30 16:06:52 -0400

    added query parameter method for level

--- code/oauth_session_nodejs/emergency.js
+++ code/oauth_session_nodejs/emergency.js
@@ -14,40 +14,49 @@ app.listen(4000, () => console.log(`App now listening on server: http://localhos
 var fs = require('fs');
 const jsonFile = './private/auth.json';
 
-// fs.readFile(jsonFile, 'utf8', function (err, data) {
-//     if (err) throw err; // we'll not consider error handling for now
-//     var jsonParsed = JSON.parse(data);
-// });
-
-// var jsonData = fs.readFileSync(jsonFile);
-// var jsonParsed = JSON.parse(jsonData);
-// console.log("Emergency Level in JSON is:" ,jsonParsed.emergency_level);
-
-
-app.get('/', (req, res) => {
+app.get('/emergency?level=0', (req, res) => {
     console.log("\nGET/ (200) Success");
     var jsonData = fs.readFileSync(jsonFile);
     var jsonParsed = JSON.parse(jsonData);
     var value = jsonParsed.emergency_level;
-    console.log("The emergency value is: " +value);
+    console.log("The emergency value is: " + value);
     res.render('pages/emergency_view', { value: value });
+    res.end();
 });
 
-app.post('/emergency', function (req, res) {
 
+app.get('/emergency', function (req, res) {
+    const level = req.query.level; //set url parameter
+    if(level >2)
+    {
+        console.log("Error");
+        return;
+        res.end();
+    }
+    
     var jsonData = fs.readFileSync(jsonFile);
     var jsonParsed = JSON.parse(jsonData);
-    // res.render('pages/emergency_view');
-    console.log("\nPOST emergency call");
-    console.log("Emergency Level in JSON is:" +jsonParsed.emergency_level);
-    // console.log(jsonParsed);
-    if (jsonParsed.emergency_level == 1) {
-        // console.log("Value of emergency in JSON is: ", +emerg_value);
-        // console.log(jsonParsed);
-        const setVal = {...jsonParsed,emergency_level:0};
-        console.log(setVal);
-        fs.writeFileSync('./private/auth.json', JSON.stringify(setVal));        
-    } else {
-        console.log("No changes needed");
-    }
+    const setVal = { ...jsonParsed, emergency_level: level };
+    fs.writeFileSync('./private/auth.json', JSON.stringify(setVal));
+    res.end();
 });
+
+
+// app.post('/emergency', function (req, res) {
+
+//     var jsonData = fs.readFileSync(jsonFile);
+//     var jsonParsed = JSON.parse(jsonData);
+//     console.log("\nPOST emergency call");
+//     console.log("Emergency Level in JSON is:" +jsonParsed.emergency_level);
+//     // console.log(jsonParsed);
+//     if (jsonParsed.emergency_level == 1) {
+// const setVal = { ...jsonParsed, emergency_level: 0 };
+//         console.log("\nPOST/ (200) Success")
+//         console.log("The value is set now to: " +setVal.emergency_level);
+// fs.writeFileSync('./private/auth.json', JSON.stringify(setVal));
+//     } else {
+//         console.log("No changes needed");
+//     }
+//     res.end();
+// });
+
